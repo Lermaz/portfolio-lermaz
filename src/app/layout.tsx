bc2cf9f9e4ea70c8dce79524c4bb1d306ee7ace5
Lermaz <lermalizarraga@gmail.com>
2025-03-17 12:47:51 -0700

    feat: :sparkles: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Nav from '@/components/nav';
 import { ViewTransitions } from 'next-view-transitions';
 import { ThemeProvider } from '@/components/theme-provider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <Nav />
             <div className="text-text dark:text-darkText mx-auto w-[750px] max-w-full px-5 pb-10 pt-28">
               {children}
+              <SpeedInsights />
             </div>
           </ThemeProvider>
         </body>
